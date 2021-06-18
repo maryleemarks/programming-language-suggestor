b7bc7fd3eafb6b888328d3fa68bfaac04025a884
Mary Marks <maryleemarks@gmail.com>
2021-06-18 13:18:16 -0700

    add branching scripts

---
+++ js/scripts.js
@@ -0,0 +1,24 @@
+$(document).ready(function() {
+  $("form#questions").submit(function(event) {
+
+    const q1 = $("input#q1").val();
+    const q2 = $("input#q2").val();
+    const q3 = $("input#q3").val();
+    const q4 = $("input#q4").val();
+    const q5 = $("input#q5").val();
+
+    if (q1 === q3 && q1 != q4) {
+      language = "Ruby"
+    } else if (q2 === q5 && q2 != q1) {
+      language = "C#"
+    } else if (q4 === q2 && q5 != q2 && q5 === q3) {
+      language = "JavaScript"
+    } else {
+      language = "not learning to code";
+    }
+    $("#answer").show();
+    $("#language").text(language);
+
+    event.preventDefault();
+  });
+});
