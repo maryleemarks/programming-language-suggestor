67d5c4ef38487fc72e6cc27ca54abb3d2bfee8f8
Mary Marks <maryleemarks@gmail.com>
2021-06-18 14:51:04 -0700

    add a name to the answer

--- js/scripts.js
+++ js/scripts.js
@@ -6,6 +6,9 @@ $(document).ready(function() {
     const q3 = $("input#q3").val();
     const q4 = $("input#q4").val();
     const q5 = $("input#q5").val();
+    const name = $("input#name").val();
+
+    $(".name").text(name);
 
     if (q1 === q3 && q1 != q4) {
       language = "Ruby"
@@ -18,6 +21,7 @@ $(document).ready(function() {
     }
     $("#answer").show();
     $("#language").text(language);
+    
 
     event.preventDefault();
   });
